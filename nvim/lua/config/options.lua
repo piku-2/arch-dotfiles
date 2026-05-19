7a17ac0a852b00408343798a89f51702df55275d
piku-2 <207675780+piku-2@users.noreply.github.com>
2026-05-20 07:21:10 +0900

    nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,10 +1,9 @@
 -- ~/.vimrc と同じ設定（LazyVim のデフォルトを上書き）
 vim.cmd("syntax on")
-vim.opt.number         = true
-vim.opt.relativenumber = false   -- vimrc にないので無効化
-vim.opt.tabstop        = 4
-vim.opt.shiftwidth     = 4
-vim.opt.cindent        = true
-vim.opt.showmatch      = true
-vim.opt.incsearch      = true
-vim.opt.hlsearch       = true
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.cindent = true
+vim.opt.showmatch = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
