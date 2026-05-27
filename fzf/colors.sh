d53fde83c93c4e42f894832597063b143da4fa6d
piku-2 <207675780+piku-2@users.noreply.github.com>
2026-05-28 08:55:55 +0900

    what

--- fzf/colors.sh
+++ fzf/colors.sh
@@ -1,2 +1,2 @@
 # Generated from: /home/watariku/.config/waybar/colors.css
-export FZF_COLOR='bg:#101417,bg+:#101417,fg:#dfe3e8,fg+:#dfe3e8,hl:#ffb4ab,hl+:#ffb4ab,prompt:#94cdf7,pointer:#94cdf7,marker:#94cdf7,border:#8b9198'
+export FZF_COLOR='bg:#111318,bg+:#111318,fg:#e2e2e9,fg+:#e2e2e9,hl:#ffb4ab,hl+:#ffb4ab,prompt:#a9c7ff,pointer:#a9c7ff,marker:#a9c7ff,border:#8e9099'
