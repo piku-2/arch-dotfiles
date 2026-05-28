26396c2346845946e89329e987b9a3c184ab2797
piku-2 <207675780+piku-2@users.noreply.github.com>
2026-05-28 19:59:07 +0900

    eww

--- eww/scripts/workspace.sh
+++ eww/scripts/workspace.sh
@@ -7,8 +7,8 @@ ws() {
   local output=""
   local workspace_data current_workspace workspace_ids windows idx
 
-  workspace_data="$(hyprctl workspaces -j)"
-  current_workspace="$(hyprctl activeworkspace -j | jq -r '.id')"
+  workspace_data="$(hypr_ipc j/workspaces)"
+  current_workspace="$(hypr_ipc j/activeworkspace | jq -r '.id')"
   mapfile -t workspace_ids < <(
     jq -r --arg current "$current_workspace" '
       ([.[] | select(.id > 0) | .id] + [($current | tonumber)])
@@ -21,7 +21,7 @@ ws() {
   for idx in "${!workspace_ids[@]}"; do
     i="${workspace_ids[$idx]}"
     windows="$(jq -r "[.[] | select(.id == ${i})] | .[0]?.windows // 0" <<<"$workspace_data")"
-    workspace_icon+=(" ${i}")
+    workspace_icon+=("${i}")
     if [[ "$current_workspace" == "$i" ]]; then
       workspace_class+=("visiting")
     elif [[ "$windows" -gt 0 ]]; then
@@ -29,18 +29,19 @@ ws() {
     else
       workspace_class+=("free")
     fi
-    if [[ "$current_workspace" == "$i" ]]; then
-      workspace_icon[$idx]=" ${i}"
-    fi
   done
 
-  output="(box :class \"ws\" :halign \"end\" :orientation \"h\" :spacing 5 :space-evenly \"false\""
+  output="(box :class \"ws\" :halign \"end\" :orientation \"h\" :spacing 0 :space-evenly \"false\""
   for idx in "${!workspace_ids[@]}"; do
     i="${workspace_ids[$idx]}"
     output+=" (eventbox :onclick \"hyprctl dispatch workspace $i\" :cursor \"pointer\" :class \"${workspace_class[$idx]}\" (label :text \"${workspace_icon[$idx]}\"))"
   done
   output+=")"
   eww update workspaces-output="$output"
+}
+
+hypr_ipc() {
+  printf '%s' "$1" | socat - "UNIX-CONNECT:$command_socket"
 }
 
 runtime_dir="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
@@ -54,11 +55,13 @@ if [ -z "$hypr_signature" ]; then
   exit 0
 fi
 
-socket="$runtime_dir/hypr/$hypr_signature/.socket2.sock"
-[ -S "$socket" ] || exit 0
+command_socket="$runtime_dir/hypr/$hypr_signature/.socket.sock"
+event_socket="$runtime_dir/hypr/$hypr_signature/.socket2.sock"
+[ -S "$command_socket" ] || exit 0
+[ -S "$event_socket" ] || exit 0
 
 ws
-stdbuf -oL socat -U - "UNIX-CONNECT:$socket" | while read -r line; do
+stdbuf -oL socat -U - "UNIX-CONNECT:$event_socket" | while read -r line; do
   case "$line" in
     workspace\>\>* | createworkspace\>\>* | destroyworkspace\>\>*)
       ws
--- nvim/lua/plugins/html_preview.lua
+++ nvim/lua/plugins/html_preview.lua
@@ -3,6 +3,5 @@ return {
 		"barrett-ruth/live-server.nvim",
 		cmd = { "LiveServerStart", "LiveServerStop", "LiveServerToggle" },
 		ft = { "html", "css", "javascript" }, -- 階層エラーが消えたので、安全にファイルタイプ検知に戻せます！
-		opts = {},
 	},
 }
